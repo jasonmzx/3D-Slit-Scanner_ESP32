5737ec5bbbb61a518e77a0c0bd4e6e7923a51d3a
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-08-10 16:44:22 -0400

    repo file structure cleanup

--- changelog.md
+++
@@ -1,4 +0,0 @@
-## Changelog / Progression-log of the Project:
-- Want to add educational value to this repository by marking milestones with dates.
-
-TODO
--- software/Arduino_ESP32/3D_Object_Scanner_ESP32/3D_Object_Scanner_ESP32/handler.h
+++
@@ -1,4 +0,0 @@
-//OLED Handlers
-void draw_start_up_logo(Adafruit_SSD1306 OLED_DISPLAY);
-void draw_menu(Adafruit_SSD1306* OLED_DISPLAY, bool selected);
-void draw_manual(Adafruit_SSD1306* OLED_DISPLAY);
